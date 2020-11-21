df41d5ba192fe40ac2f9803bab2442266f92994f
dasha-ryzhkova <ryzhkovadasha2002@gmail.com>
2020-11-21 20:18:41 +0200

    added

---
+++ src/main/java/com/CSVParser.java
@@ -0,0 +1,191 @@
+package com;
+
+
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CSVParser{
+    String inputFile;
+    String outputFile;
+    char delimiter;
+    char combiner;
+    char newdel;
+    Integer numb;
+    ArrayList<ArrayList<String>> linesArr = new ArrayList<>();
+    ArrayList<String> wordsOfLine = new ArrayList<>();
+    ArrayList<String>  lines;
+
+
+    public CSVParser(String inputFile, char delimiter, char combiner, char newdel, Integer numb) {
+        this.inputFile = inputFile;
+        this.delimiter = delimiter;
+        this.combiner = combiner;
+        this.newdel = newdel;
+        this.numb = numb;
+    }
+
+    public CSVParser() {
+
+    }
+
+
+    public ArrayList<ArrayList<String>> readCSVFile() throws IOException{
+        lines = new ArrayList<>();
+        Path path = Paths.get(inputFile);
+        long lineCount = Files.lines(path).count();
+        int size = (int)(lineCount / numb + 1);
+        int distance = 0;
+
+        FileInputStream str = new FileInputStream(inputFile);
+        FileChannel chnl = str.getChannel();
+
+        for(int i = 0; i < numb; i++){
+            if(lineCount < i*size + size){
+                distance =(int)(lineCount - i*size);
+            }else{
+                distance = size;
+            }
+            OpenFile openFile = new OpenFile(i * size, distance, chnl);
+            Thread thread = new Thread(openFile);
+            thread.start();
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        str.close();
+        parseCSV(lines);
+        return linesArr;
+    }
+
+
+
+    public void parseCSV(ArrayList<String> lines) {
+        for (String s : lines) {
+            String cleanedline = deleteQuotesOfLine(s);
+            wordsOfLine = new ArrayList<>();
+            while (cleanedline.length() > 0) {
+                cleanedline = extractWordFromLine(cleanedline);
+            }
+            linesArr.add(wordsOfLine);
+        }
+    }
+
+    public void writeToFile(String outputFile, ArrayList<ArrayList<String>> filesLinesArray, char newdelimitr) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));) {
+            for (ArrayList<String> l : filesLinesArray) {
+                for (int i = 0; i < l.size(); i++) {
+                    if (i < l.size() - 1) {
+                        writer.write(l.get(i).length() + "" + newdelimitr);
+                    } else {
+                        writer.write(l.get(i).length() + "\n");
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String extractWordFromLine(String line) {
+        int i = 0;
+        String word = "";
+        if(line.charAt(i) == '\uFEFF'){
+            i++;
+        }
+        if(line.charAt(0) == combiner) {
+            do{
+                word += line.charAt(i);
+                i++;
+            }while (i != line.length() && line.charAt(i) != delimiter || line.charAt(i - 1) != combiner);
+            word = deleteQuotesOfLine(word);
+            word = deleteQuotesOfLine(word);
+            word = deleteDoubleQuotesInWord(word);
+            wordsOfLine.add(word);
+            while (i < line.length() - 1 && line.charAt(i) == delimiter) {
+                i++;
+            }
+            if (i < line.length()) {
+                line = line.substring(i);
+            } else {
+                line = "";
+            }
+        } else {
+            do {
+                word += line.charAt(i);
+                i++;
+            } while (i != line.length() && line.charAt(i) != delimiter);
+            word = deleteDoubleQuotesInWord(word);
+            wordsOfLine.add(word);
+            while (i < line.length() - 1 && line.charAt(i) == delimiter) {
+                i++;
+            }
+            if (i < line.length()) {
+                line = line.substring(i);
+            } else {
+                line = "";
+            }
+        }
+        return line;
+    }
+
+
+    public String deleteQuotesOfLine(String str) {
+        if (str.length() > 1 && str.charAt(0) == combiner && str.charAt(str.length() - 1) == combiner) {
+            str = str.substring(1, str.length() - 1);
+        }
+        return str;
+    }
+
+    public String deleteDoubleQuotesInWord(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == combiner && str.charAt(i + 1) == combiner) {
+                result += combiner;
+                i++;
+            } else {
+                result += str.charAt(i);
+            }
+        }
+        return result;
+    }
+
+    public class OpenFile implements Runnable {
+        private FileChannel channel;
+        private int startLocation;
+        private int distance;
+        private ArrayList<String> temp;
+
+        public OpenFile(int startLocation, int distance, FileChannel chnl) {
+            this.startLocation = startLocation;
+            this.distance = distance;
+            this.channel = chnl;
+        }
+
+
+        public void run() {
+            temp = new ArrayList<>();
+            try (Stream<String> lines = Files.lines(Paths.get(inputFile))) {
+                for(int i = startLocation; i < startLocation + distance; i++){
+                    String line = Files.readAllLines(Paths.get(inputFile)).get(i);
+                    temp.add(line);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            lines.addAll(temp);
+        }
+    }
+}
+
---
+++ src/main/java/com/Main.java
@@ -0,0 +1,52 @@
+package com;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)throws IOException
+    {
+        ArrayList<ArrayList<String>> filesLinesArray = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter catalog path:");
+
+        String folderPath = "C:\\Users\\VivoBook\\Desktop\\4";
+        File folder = new File(folderPath);
+        File[] listOfFiles = folder.listFiles();
+        System.out.println("Enter delimiter:");
+        //String del1 = scanner.next();
+        String del1 = ",";
+
+        System.out.println("Enter new delimiter:");
+        //String newdel1 = scanner.next();
+        String newdel1 = "+";
+
+        System.out.println("Enter path for new file:");
+        //String newpath = scanner.next();
+        String newpath = "C:\\Users\\VivoBook\\Desktop\\result.txt";
+        char del = del1.charAt(0);
+        char newdel = newdel1.charAt(0);
+
+        System.out.println("Enter count of thread:");
+        //Integer numb = scanner.nextInt();;
+        Integer numb = 7;
+
+        for (File file : listOfFiles) {
+            if (file.isFile()) {
+                CSVParser parser = new CSVParser(folderPath + file, del, '"', newdel, numb);
+                filesLinesArray.addAll(parser.readCSVFile());
+            }
+        }
+        CSVParser parser = new CSVParser();
+        parser.writeToFile(newpath, filesLinesArray, newdel);
+
+        scanner.close();
+    }
+}
+
+// C:\Users\VivoBook\Desktop\lab1.csv
+// C:\Users\VivoBook\Desktop\result.txt
